2de9d486ee951a2670fe0b1fe1334e79c68813eb
Devan Kavalchek <devankav7@gmail.com>
2022-02-15 23:51:55 -0500

    added board functionality

--- app/src/main/java/com/devankav/tictactoe/Board.kt
+++ app/src/main/java/com/devankav/tictactoe/Board.kt
@@ -7,11 +7,19 @@ class Board {
         arrayOf(Tile.EMPTY, Tile.EMPTY, Tile.EMPTY)
     )
 
-    fun tileOpen(row: Int, column: Int): Boolean {
-        return false
+    private fun tileOpen(row: Int, column: Int) = board[row][column] == Tile.EMPTY
+
+    fun takeTile(row: Int, column: Int, tileSymbol: Tile) {
+        if (tileOpen(row, column)) {
+            board[row][column] = tileSymbol
+        }
     }
 
-    fun takeTile(row: Int, column: Int) {
-        
+    fun clearTable() {
+        for (row: Array<Tile> in board) {
+            for (i in 0..2) {
+                row[i] = Tile.EMPTY
+            }
+        }
     }
 }
