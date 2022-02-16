22ca24b79e86f63e5fe9b46b9b436a6c4677d74e
Devan Kavalchek <devankav7@gmail.com>
2022-02-15 23:38:41 -0500

    updated activity

--- app/src/main/java/com/devankav/tictactoe/MainActivity.kt
+++ app/src/main/java/com/devankav/tictactoe/MainActivity.kt
@@ -1,11 +1,35 @@
 package com.devankav.tictactoe
 
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+
+private const val TAG = "MainActivity"
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val buttons = listOf<Button>(
+            findViewById(R.id.button1),
+            findViewById(R.id.button2),
+            findViewById(R.id.button3),
+            findViewById(R.id.button4),
+            findViewById(R.id.button5),
+            findViewById(R.id.button6),
+            findViewById(R.id.button7),
+            findViewById(R.id.button8),
+            findViewById(R.id.button9),
+        )
+
+        buttons.forEach {button ->
+            button.setOnClickListener(View.OnClickListener { view ->
+                val id = view.id
+                Log.v(TAG, resources.getResourceName(id))
+            })
+        }
     }
 }
